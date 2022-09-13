a99a7f579003ffcd224b529a34006fa74b287149
michael <mikegomez122@gmail.com>
2022-09-12 23:21:58 -0600

    Fully tested and refectored

--- Language Recongnizer.kt
+++ Language Recongnizer.kt
@@ -9,113 +9,169 @@ class languageRecognizer constructor(input : String) {
         var ierrFlag : Boolean = false
         /*
         Flags
-           -1 - terminates the recursion
+           -1 - terminates the recursion and returns errFlag
             0 - checks the edges of the string, as well as empty instruction
-            1 - checks for the first variable
-            2 - checks for the assignment variable
-            3 - checks for the assignment variable and then peaks at the next operator / ; / stop lexemes
+            1 - checks for the variable
+            2 - checks for the assignment token
+            3 - checks for the first assigned variable
+            4 - checks if an expression is being used
         */
+
+        //checking for gibberish input
+        for(i in sen){
+            if (i.length > 5){
+                println("gibberish has been detected, please read the syntax")
+                return true
+            }
+        }
 
         if(flag != -1){
             //Recursive flow control
             when(flag){
-                -1 -> {
-                    //ending the recursion
-                    return errFlag
-                }
-
-                //checking the edges and for emtpy instruction
+                //checking the edges and for empty instruction
                 0 -> {
                     //checking for start
                     if (sen.first() != "start") {
                         println("syntax error: expected \"start\" instead of \"" + sen.first() + "\"")
                         ierrFlag = true
                     }
+
+                    //checking if the list only consists of one string
+                    if(sen.size == 1){
+                        println("incomplete instruction")
+                        return checkSentence(sen, -1, true, 0)
+                    }
+
                     //checking for stop
                     if (sen.last() != "stop") {
                         println("syntax error: expected \"stop\" instead of \"" + sen.last() + "\"")
                         ierrFlag = true
                     }
+
                     //checking for empty instructions
-                    if (sen.get(1) == "stop") {
+                    if (sen.get(1) == sen.last()) {
                         println("no instructions to compile")
                         return checkSentence(sen,-1, true, 0)
                     }
 
                     //next
-                    return  checkSentence(sen, 1, (ierrFlag || errFlag), index + 1)
+                    return  checkSentence(sen, 1, ierrFlag, index + 1)
                 }
 
                 //checking for the first variable
                 1 -> {
-                    if(sen.get(index) != "stop" || sen.get(index) != sen.last())
+                    //checking if the correct variable is being used
+                    if(!isValidVariable(sen.get(index)))
                     {
-                        if(sen.get(index) == "R" || sen.get(index) == "S" || sen.get(index) == "T" || sen.get(index) == "U" )
-                        {
-                            checkSentence(sen, 2, errFlag, index + 1)
-                        }
-                        else{
-                            println("syntax error: expected \"R\",\"S\",\"T\",\"U\", instead of \"" + sen.get(index) + "\"")
-                            ierrFlag = true
-                            return checkSentence(sen, 2, (ierrFlag || errFlag), index + 1)
-                        }
+                        println("syntax error: expected \"R\",\"S\",\"T\",\"U\", instead of \"" + sen.get(index) + "\"")
+                        ierrFlag = true
                     }
+
+                    //check if the next string is 'stop'
+                    if(isStopNext(sen.get(index + 1), sen.last()))
+                    {
+                        println("syntax error: incomplete instruction")
+                        return checkSentence(sen, -1, true, index + 1)
+                    }
+
+                    //next
+                    return checkSentence(sen, 2, (ierrFlag || errFlag), index + 1)
                 }
 
                 //checks the assignment token
                 2 -> {
-                    if(sen.get(index) != "stop" || sen.get(index) != sen.last())
+                    //checking for the assignment token
+                    if(sen.get(index) != "<=")
                     {
-                        if(sen.get(index) != "<=")
-                        {
-                            println("syntax error: expected \"<=\" instead of \"" + sen.get(index) + "\"")
-                            ierrFlag = true
-                        }
-                        return checkSentence(sen, 3, (ierrFlag || errFlag), index + 1)
+                        println("syntax error: expected \"<=\" instead of \"" + sen.get(index) + "\"")
+                        ierrFlag = true
                     }
+
+                    //check if the next string is 'stop'
+                    if(isStopNext(sen.get(index + 1), sen.last()))
+                    {
+                        println("syntax error: incomplete instruction")
+                        return checkSentence(sen, -1, true, index + 1)
+                    }
+
+                    //next
+                    return checkSentence(sen, 3, (ierrFlag || errFlag), index + 1)
                 }
 
-                //checking the assigned variable
+                //checking if assignment expression syntax is being followed
                 3 -> {
-                    if(sen.get(index) != "stop" || sen.get(index) != sen.last())
+                    //checking for the first assigned variable
+                    if(!isValidVariable(sen.get(index)))
                     {
-                        //checking if it follows syntax
-                        if(sen.get(index) == "R" || sen.get(index) == "S" || sen.get(index) == "T" || sen.get(index) == "U" )
-                        {
-                            //next
+                        println("syntax error: expected \"R\",\"S\",\"T\",\"U\", instead of \"" + sen.get(index) + "\"")
+                        ierrFlag = true
+                    }
 
-                            //checking if next is a token, ; , or stop
-                            if(sen.get(index + 1) == "/" || sen.get(index + 1) == "*")
-                            {
-                                checkSentence(sen, 3, errFlag, index + 2)
-                            }
-                            else if (sen.get(index + 1) == ";") {
-                                checkSentence(sen, 1, errFlag, index + 2)
-                            }
-                            else if (sen.get(index + 1) == "stop"){
-                                checkSentence(sen, -1, errFlag, index + 1)
-                            }
+                    //next
+                    return checkSentence(sen, 4, (ierrFlag || errFlag), index + 1)
+                }
+
+                //checking if an expression token is being used
+                4 -> {
+                    //checking which expression token is being used
+                    /* flags
+                       -2 = not following syntax
+                       -1 = " stop " is the current lexeme
+                        0 = " / " or " * " is the current token
+                        1 = " ; " is the current token
+                     */
+                    when (whatExpressionToken(sen.get(index))){
+                        //an expression is being used
+                        0 -> {
+                            return checkSentence(sen, 3, errFlag, index + 1)
+                        }
+
+                        //not a token or stop; something not following the syntax
+                        -2 -> {
                             //reporting that we don't know wtf is there
-                            else{
-                                println("syntax error: expected operation ( \"/\" or \"*\" ) or expected \";\" or  expected \"stop\"")
-                                checkSentence(sen, 1, true, index + 1)
-                            }
+                            println("syntax error: expected operation ( \"/\" or \"*\" ) or expected \";\" or  expected \"stop\"")
+                            println("stopping compilation")
+                            return checkSentence(sen, -1, true, index + 1)
+                        }
 
-                            //end of next
-                        }
-                        //reporting that the variable doesn't follow the syntax
-                        else{
-                            println("syntax error: expected \"R\",\"S\",\"T\",\"U\", instead of \"" + sen.get(index) + "\"")
-                            ierrFlag = true
-                            return checkSentence(sen, 3, (ierrFlag || errFlag), index + 1)
-                        }
+                        // completed assignment
+                        1 -> return checkSentence( sen, 1, errFlag, index + 1)
+
+                        //normal assignment is preformed and instructions are completed
+                        -1 -> return checkSentence(sen, -1, errFlag, index)
                     }
                 }
             }
 
         }
 
-        return (ierrFlag || errFlag)
+        return errFlag
+    }
+
+    //internal helper functions
+    private fun isStopNext(s : String, l : String) : Boolean {
+        if (s == "stop" ||  s == l){
+            return true
+        }
+        return false
+    }
+
+    private fun isValidVariable(s : String): Boolean{
+        if (s == "R" || s == "S" || s == "T" || s == "U" )
+        {
+            return true
+        }
+        return false
+    }
+
+    private fun whatExpressionToken(s : String): Int{
+        return when(s){
+            "stop" -> -1
+            "/" -> 0
+            "*" -> 0
+            ";" -> 1
+            else -> -2
+        }
     }
 
     //returns the formatted List<String>
